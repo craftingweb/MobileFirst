9255b5c4d14abaf170de852aeaedf26e39ea27a3
VitaliPri <vitaliy.prymak@cix.csi.cuny.edu>
2024-10-22 17:31:56 -0400

    header @600, 768, 1024, 1440

--- js/main.js
+++ js/main.js
@@ -3,6 +3,10 @@ $(function () {
     dots: true,
     slidesToShow: 1,
     slidesToScroll: 1,
+    nextArrow:
+      '<button type="button" class="slick-next slick-arrow"><img src="/images/back.svg"></button>',
+    prevArrow:
+      '<button type="button" class="slick-prev slick-arrow"><img src="/images/forward.svg"></button>',
     responsive: [
       {
         breakpoint: 1024,
